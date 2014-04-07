5568ff2209efd68f70889f765b69923b3fa8051c
Constantin Wildförster <constantin@wildfoerster.org>
2014-04-07 19:35:22 +0200

    just some random hacking

---
+++ js/yzal.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+	var socket = io.connect('/');
+	socket.on('update', function updateUi(data) {
+		// remove all items
+		$('.col-md-4').html('');
+
+		// fill with 'new' ones
+		for (var i = 0; i < data.length; i++) {
+			var $c = $('.col-md-4:eq(' + (i % 3) + ')');
+			$c.append('<h2>' + data[i].name + '</h2>');
+
+			for (var j = 0; j < data[i].items.length; j++) {
+				var item = data[i].items[j];
+
+				$c.append('<div class="btn-container"><span class="switch btn ' + (item.state ? 'btn-success' : 'btn-default') + (typeof(item.timer) != "undefined" && item.timer.length > 0 ? ' has-timer' : '') + '" data-system="' + item.system + '" data-remote="' + item.remote + '"><span class="glyphicon glyphicon-time"></span><i class="glyphicon glyphicon-off"></i><strong>' + item.name + '</strong></span></div>');
+			}
+		}
+
+		// set click handlers
+		$('.col-md-4 .switch').longpress(function(e) {
+			var $t = $('#timer-modal');
+			$t.attr('data-system', $(this).attr('data-system'));
+			$t.attr('data-remote', $(this).attr('data-remote'));
+			$t.modal('show');
+		}, function(e) {
+			$(this).addClass('disabled');
+			socket.emit('switch', { system: $(this).attr('data-system'), remote: $(this).attr('data-remote'), state: ($(this).hasClass('btn-success') ? 0 : 1) });
+		});
+	});
+
+	$('#master-off').click(function() {
+		$('.col-md-4 .switch').addClass('disabled');
+		socket.emit('master-off', {});
+	});
+
+	$('#timer-modal .btn-primary').click(function(e) {
+		e.preventDefault();
+
+		var $m = $('#timer-modal');
+		var system = $m.attr('data-system');
+		var remote = $m.attr('data-remote');
+		var mode = 0; //$('.timer-mode .active input').val();
+		var type = $('.timer-type .active input').val();
+		var $t1 = $('#timer-time');
+
+		if ($t1.val() === '') {
+			$t1.parent().parent().addClass('has-error');
+		} else {
+			$t1.parent().parent().removeClass('has-error');
+			socket.emit('timer', { system: $m.attr('data-system'), remote: $m.attr('data-remote'), mode: mode, type: type, time: $t1.val() });
+			$m.modal('hide');
+			$t1.val('');
+		}
+	})
+});
---
+++ server.js
@@ -0,0 +1,174 @@
+var app    = require('http').createServer(handler),
+    io     = require('socket.io').listen(app, { log: false }),
+    fs     = require('fs'),
+    path   = require('path'),
+    url    = require('url'),
+    sh     = require('execSync'),
+    config = require('./config.json');
+
+// simple mimetypes...
+var mimeTypes = {
+  'html' : 'text/html',
+  'js'   : 'text/javascript',
+  'css'  : 'text/css',
+  'svg'  : 'image/svg+xml',
+  'ttf'  : 'application/x-font-ttf',
+  'otf'  : 'application/x-font-opentype',
+  'woff' : 'application/font-woff',
+  'eot'  : 'application/vnd.ms-fontobject'
+};
+
+app.listen(config.port);
+
+// super simple server, maybe use connect or something like that?
+function handler(req, res) {
+  var uri = url.parse(req.url).pathname;
+  if (uri === '/') {
+    uri = '/client.html';
+  }
+
+  var filename = path.join(process.cwd(), uri);
+  
+  fs.exists(filename, function(exists) {
+    // 404 Not Found
+    if (!exists) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.write('404 Not Found\n');
+      res.end();
+      return;
+    }
+
+    var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+    res.writeHead(200, { 'Content-Type': mimeType });
+
+    fs.createReadStream(filename).pipe(res);
+  });
+}
+
+// checks for timers in our items and triggers them if the time has passed...
+function checkTimer() {
+  var now = new Date().getTime(),
+      switched = false;
+
+  for (var i = 0; i < config.groups.length; i++) {
+    for (var j = 0; j < config.groups[i].items.length; j++) {
+      var item = config.groups[i].items[j];
+
+      if (typeof(item.timer) != "undefined" && item.timer.length > 0) {
+        for (var k = item.timer.length -1; k >= 0 ; k--){
+          // check if the time has passed...
+          if (now > item.timer[k].time) {
+            var state = parseInt(item.timer[k].type == 2 ? (item.state == 1 ? 0 : 1) : item.timer[k].type);
+            switchInternal(item, state);
+            item.timer.splice(k, 1);
+            switched = true;
+          }
+        }
+      }
+    }
+  }
+
+  if (switched) {
+    updateAllClients();
+  }
+
+  setTimeout(checkTimer, 1000);
+}
+
+// check forever...
+checkTimer();
+
+// get the specific remote item
+function getItem(system, remote) {
+  for (var i = 0; i < config.groups.length; i++) {
+    for (var j = 0; j < config.groups[i].items.length; j++) {
+      var item = config.groups[i].items[j];
+      if (item.system == system && item.remote == remote) {
+        return item;
+      }
+    }
+  }
+
+  console.log('invalid item: ' + system + ',' + remote);
+  return null;
+}
+
+// does the low level switching
+function switchInternal(item, state) {
+  sh.run(config.command + ' ' + item.system + ' ' + item.remote + ' ' + state);
+  item.state = state;
+}
+
+// updates all clients...
+function updateAllClients() {
+  saveConfig();
+  io.sockets.emit('update', config.groups);
+}
+
+// save config
+function saveConfig() {
+  fs.writeFile('config.json', JSON.stringify(config, null, 2), function(err) {
+    if (err) {
+      console.log('Failed to save config.json: ' + err);
+    }
+  }); 
+}
+
+io.sockets.on('connection', function(socket) {
+  // update client upon connection
+  socket.emit('update', config.groups);
+
+  // register switch event
+  socket.on('switch', function(data) {
+    var item = getItem(data.system, data.remote);
+    if (item != null) {
+      switchInternal(item, data.state);
+      updateAllClients();
+    }
+  });
+
+  socket.on('timer', function(data) {
+    var item = getItem(data.system, data.remote);
+    if (item != null) {
+
+      // make sure our config is able to push timers
+      var timer;
+      if (typeof(item.timer) != "undefined") {
+        timer = item.timer;
+      } else {
+        timer = new Array();
+        item.timer = timer;
+      }
+
+      // calculate our trigger time
+      var time;
+      var v = data.time.split(':');
+      if (data.mode == 0) {
+        time = v[0] * 24 * 60 * 1000 + v[1] * 60 * 1000;
+      } else {
+        // TODO calculate time..
+      }
+      
+      // add timer
+      timer.push({
+        mode: data.mode,
+        type: data.type,
+        time: new Date().getTime() + time
+      });
+
+      updateAllClients();
+    }
+  });
+
+  // register master off event. we just force ALL items to state 0!
+  socket.on('master-off', function(unused) {
+    for (var i = 0; i < config.groups.length; i++) {
+      for (var j = 0; j < config.groups[i].items.length; j++) {
+        var item = config.groups[i].items[j];
+        switchInternal(item, 0);
+      }
+    }
+
+    updateAllClients();
+  });
+});
